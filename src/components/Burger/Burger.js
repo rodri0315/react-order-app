27aa82798d6e3dfc24d3640c2e05dcd8d5d00afe
rodri0315 <rodri0315@gmail.com>
2019-12-21 01:43:58 -0500

    Add state to BurgerBuilder, add builder control to Burger.js

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,29 @@ import React from "react";
 import classes from "./Burger.module.css";
 import BurgerIngredient from "../Burger/BurgerIngredients/BurgerIngredient";
 
-const Burger = () => {
+const Burger = ({ ingredients }) => {
+  // Will always be an Array, even an array with empty elements.
+  let transformedIngredients = Object.keys(ingredients)
+    .map(ingredientKey => {
+      console.log("Ingredients Key", ingredientKey);
+      console.log("Ingredients Value", ingredients[ingredientKey]);
+      return [...Array(ingredients[ingredientKey])].map((_, index) => {
+        return (
+          <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />
+        );
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  console.log(transformedIngredients);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please add ingredients!</p>;
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type={"bread-top"} />
-      <BurgerIngredient type={"cheese"} />
-      <BurgerIngredient type={"meat"} />
+      {transformedIngredients}
       <BurgerIngredient type={"bread-bottom"} />
     </div>
   );
